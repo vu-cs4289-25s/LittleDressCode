6899683ea8ed674bf2c9f4c09e35676fbd95b935
Nia Yick <niayick@Nias-MacBook-Pro.local>
2025-03-26 14:09:21 -0500

    changes for env

--- frontend/app/utils/firebaseConfig.js
+++ frontend/app/utils/firebaseConfig.js
@@ -1,25 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBXPbxNcJ-l7AD9D9ymHPrpIycGaYjpUoQ",
-  authDomain: "littledresscode-1cd43.firebaseapp.com",
-  projectId: "littledresscode-1cd43",
-  storageBucket: "littledresscode-1cd43.firebasestorage.app",
-  messagingSenderId: "188376730453",
-  appId: "1:188376730453:web:593f3e87b0846b17493ddc",
-  measurementId: "G-YMV25MY59Z",
-  webClientId:
-    "188376730453-pqu8qc10oherj7pedlqa58jecqkafd93.apps.googleusercontent.com",
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
